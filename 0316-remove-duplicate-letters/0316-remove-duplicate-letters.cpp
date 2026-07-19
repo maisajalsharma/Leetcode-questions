9e439628feb8baa069adb071f601c8a573c8102e
sajal sharma <2k24.cs1n.2413793@gmail.com>
2026-07-19 17:02:27 +0530

    Time: 0 ms (100%), Space: 9.9 MB (13.03%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        
+        int n = s.size();
+        stack<char>st;
+        unordered_map<int,int>m;
+        vector<int>st_freq(26,0);
+
+        for(int i = 0; i < s.size();i++)
+        {
+            m[s[i]]++;
+        }
+
+        for(int i = 0; i < n;i++)
+        {
+            m[s[i]]--;
+
+            if(st_freq[s[i] - 'a']) continue;
+
+            while(!st.empty() && st.top() > s[i] && m[st.top()] > 0)
+            {
+                if(m[st.top()] > 0)
+                {
+                    st_freq[st.top() - 'a'] = 0;
+                    st.pop();   
+                }
+            }
+
+            st.push(s[i]);
+            st_freq[s[i] - 'a'] = 1;
+        }
+
+        string result = "";
+
+        while(!st.empty())
+        {
+            result = st.top() + result;
+            st.pop();
+        }
+
+        return result;
+    }
+};
